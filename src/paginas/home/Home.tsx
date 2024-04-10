eb2a67f2102b591039b5d8b4d827ab52aaecca02
NandayGB <fernandagbarbosa97@gmail.com>
2024-04-09 22:38:44 -0300

    first commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://br.pinterest.com/pin/638033472230413559/" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
